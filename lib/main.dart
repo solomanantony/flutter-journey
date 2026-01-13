51b6c4c863a529b874e764c2b694e250fd38fe8d
Soloman Antony <solomanantony123@gmail.com>
2026-01-13 19:10:55 +0530

    flutter app 8

---
+++ lib/main.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_application_8/login.dart';
+void main(){
+  runApp(MyApp());
+}
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(home:LoginPage(),
+    debugShowCheckedModeBanner: false,);
+  }
+}
